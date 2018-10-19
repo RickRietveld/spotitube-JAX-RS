6c9a78993be541c0a2a6ca03c85db7960a389373
RickRietveld <rickrietveld10@Hotmail.com>
2018-10-19 15:07:05 +0200

    All functionalities implemented

---
+++ src/main/java/nl/han/oose/service/login/LoginServiceImpl.java
@@ -0,0 +1,40 @@
+package nl.han.oose.service.login;
+
+import nl.han.oose.entity.account.Account;
+import nl.han.oose.entity.account.UserToken;
+import nl.han.oose.persistence.account.TokenDAO;
+import nl.han.oose.persistence.account.UserDAO;
+
+import javax.enterprise.inject.Default;
+import javax.inject.Inject;
+import javax.security.auth.login.LoginException;
+
+@Default
+public class LoginServiceImpl implements LoginService {
+
+    @Inject
+    private UserDAO userDAO;
+
+    @Inject
+    private TokenDAO tokenDAO;
+
+
+    @Override
+    public UserToken login(Account account) throws LoginException {
+
+        if (tokenDAO.checkIfTokenAlreadyExists(account.getUser())) {
+            return tokenDAO.getExistingUserAndToken(account.getUser());
+        } else if (userDAO.verifyLogin(account)) {
+            return tokenDAO.getNewToken(account);
+        } else {
+            throw new LoginException("Wrong credentials");
+        }
+    }
+
+
+}
+
+
+
+
+
