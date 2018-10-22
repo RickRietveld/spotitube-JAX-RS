57be87ff22ea76527ce8e052b3d030016657fe69
RickRietveld <rickrietveld10@Hotmail.com>
2018-10-22 16:13:27 +0200

    All unit tests finished

--- database/spotitube-DDL.sql
+++ database/spotitube-DDL.sql
@@ -1,35 +1,49 @@
 create table playlist
 (
-	id int(3) not null,
-	name varchar(25) not null,
-	owner varchar(25) not null
-)
-;
-
-create table song
-(
-	id int(3) not null,
-	title varchar(75) not null,
-	performer varchar(30) not null,
-	duration int(4) not null,
-	album varchar(50) not null,
-	offlineAvailable tinyint(1) not null
+	id int auto_increment
+		primary key,
+	name varchar(255) not null,
+	user varchar(255) not null,
+	owner tinyint(1) not null
 )
 ;
 
 create table token
 (
-	token varchar(15) not null
+	token varchar(255) not null
+		primary key,
+	user varchar(255) not null,
+	expiryDate date not null
 )
 ;
 
 create table track
 (
-	id int(3) not null,
-	title varchar(75) not null,
-	performer varchar(30) not null,
-	duration int(4) not null,
+	id int(255) not null
+		primary key,
+	title varchar(255) not null,
+	performer varchar(255) not null,
+	duration int(255) not null,
+	album varchar(255) not null,
+	playcount int(255) not null,
+	publicationDate varchar(255) not null,
+	description varchar(255) not null,
 	offlineAvailable tinyint(1) not null
+)
+;
+
+create table trackPlaylistRelation
+(
+	playlistId int(255) not null,
+	trackId int(255) not null,
+	offlineAvailable tinyint(1) not null,
+	primary key (playlistId, trackId),
+	constraint fk_trackPlaylistRelation_playlist
+		foreign key (playlistId) references playlist (id)
+			on update cascade on delete cascade,
+	constraint fk_trackPlaylistRelation_track
+		foreign key (trackId) references track (id)
+			on update cascade on delete cascade
 )
 ;
 
@@ -37,21 +51,11 @@ create table user
 (
 	user varchar(255) not null
 		primary key,
-	password varchar(255) not null,
-	token varchar(255) not null
+	password varchar(255) not null
 )
 ;
 
-create table video
-(
-	id int(3) not null,
-	title varchar(75) not null,
-	performer varchar(30) not null,
-	duration int(4) not null,
-	playcount int(5) not null,
-	publicationDate date not null,
-	description varchar(100) not null,
-	offlineAvailable tinyint(1) not null
-)
-;
+
+
+
 
